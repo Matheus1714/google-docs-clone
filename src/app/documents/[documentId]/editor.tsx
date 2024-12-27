e923f7832a3e2a67545dda13706586f1b1c9bc2a
matheus1714 <matheussilvamartins1714@gmail.com>
2024-12-27 11:23:22 -0300

    feat: create ruler to control margins

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -23,79 +23,81 @@ import { FontSizeExtension } from '@/app/extensions/font-size';
 import { LineHeightExtension } from '@/app/extensions/line-height';
 
 import { useEditorStore } from '@/store/use-editor-store';
-import { types } from 'util';
+import { Ruler } from './ruler';
 
 export const Editor = () => {
   const { setEditor } = useEditorStore();
 
   const editor = useEditor({
-      onCreate({ editor }) {
-        setEditor(editor);
-      },
-      onDestroy(props) {
-        setEditor(null);
-      },
-      onUpdate({ editor }) {
-        setEditor(editor);
-      },
-      onSelectionUpdate({ editor }) {
-        setEditor(editor);
-      },
-      onTransaction({ editor }) {
-        setEditor(editor);
-      },
-      onFocus({ editor }) {
-        setEditor(editor);
-      },
-      onBlur({ editor }) {
-        setEditor(editor);
-      },
-      onContentError({ editor }) {
-        setEditor(editor);
-      },
-      editorProps: {
-          attributes: {
-              style: "padding-left: 56px; padding-right: 56px;",
-              class: "focus:outline-none border print:border-0 bg-white border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-10 pb-10 cursor-text"
-          }
-      },
-      extensions: [
-        LineHeightExtension,
-        FontSizeExtension,
-        TextAlign.configure({
-          types: ['heading', 'paragraph'],
-        }),
-        Link.configure({
-          openOnClick: false,
-          autolink: true,
-          defaultProtocol: "https",
-        }),
-        Color,
-        Highlight.configure({ multicolor: true }),
-        FontFamily,
-        TextStyle,
-        Underline,
-        StarterKit,
-        TaskList,
-        TaskItem.configure({
-            nested: true,
-        }),
-        Table.configure({
-            resizable: true,
-        }),
-        TableRow,
-        TableHeader,
-        TableCell,
-        Image,
-        Dropcursor,
-        ImageResize,
-      ],
+    immediatelyRender: false,
+    onCreate({ editor }) {
+      setEditor(editor);
+    },
+    onDestroy(props) {
+      setEditor(null);
+    },
+    onUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onSelectionUpdate({ editor }) {
+      setEditor(editor);
+    },
+    onTransaction({ editor }) {
+      setEditor(editor);
+    },
+    onFocus({ editor }) {
+      setEditor(editor);
+    },
+    onBlur({ editor }) {
+      setEditor(editor);
+    },
+    onContentError({ editor }) {
+      setEditor(editor);
+    },
+    editorProps: {
+        attributes: {
+            style: "padding-left: 56px; padding-right: 56px;",
+            class: "focus:outline-none border print:border-0 bg-white border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-10 pb-10 cursor-text"
+        }
+    },
+    extensions: [
+      LineHeightExtension,
+      FontSizeExtension,
+      TextAlign.configure({
+        types: ['heading', 'paragraph'],
+      }),
+      Link.configure({
+        openOnClick: false,
+        autolink: true,
+        defaultProtocol: "https",
+      }),
+      Color,
+      Highlight.configure({ multicolor: true }),
+      FontFamily,
+      TextStyle,
+      Underline,
+      StarterKit,
+      TaskList,
+      TaskItem.configure({
+          nested: true,
+      }),
+      Table.configure({
+          resizable: true,
+      }),
+      TableRow,
+      TableHeader,
+      TableCell,
+      Image,
+      Dropcursor,
+      ImageResize,
+    ],
   });
   return (
-      <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible'>
-          <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
-              <EditorContent editor={editor} />
-          </div>
+    <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible'>
+      <Ruler />
+      <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
+          <EditorContent editor={editor} />
       </div>
+    </div>
   )
 }
