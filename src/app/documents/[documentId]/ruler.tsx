0e53434e513f35e7cb93a4f6e4167ad7695de5a4
matheus1714 <matheussilvamartins1714@gmail.com>
2024-12-27 15:29:31 -0300

    feat: create methods navbar

--- src/app/documents/[documentId]/ruler.tsx
+++ src/app/documents/[documentId]/ruler.tsx
@@ -63,7 +63,7 @@ export const Ruler = () => {
             onMouseMove={handleMouseMove}
             onMouseUp={handleMouseUp}
             onMouseLeave={handleMouseUp}
-            className="h-6 border-b border-gray-300 flex items-end relative select-none print:hidden"
+            className="w-[816px] mx-auto h-6 border-b border-gray-300 flex items-end relative select-none print:hidden"
         >
             <div
                 id="ruler-container"
